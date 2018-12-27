5c9c61967bb1a59f8bdb1830305f502d2b2088bc
Alex Klein <alexdavidklein@gmail.com>
2018-12-27 13:04:34 -0800

    add docstrings to BaseLayer

--- layer.py
+++ layer.py
@@ -14,28 +14,86 @@ class BaseLayer():
         return self.weights[key]
     
     def __call__(self, *args, **kwargs):
+        """Alternate call method for activate."""
         return self.activate(*args, **kwargs)
     
     def _construct(self, dim):
+        """
+        Initialize weights and set input_shape according to supplied dimension.
+
+        Takes:
+            dim: new input_shape for layer.
+        """
         self.input_shape=dim
         self.weights=np.random.uniform(-1, 1, (self.input_shape, self.output_shape))
 
     def activate(self, X):
+        """
+        Apply activation function to array X.
+        
+        Parameters
+        ----------
+        X: ndarray of shape (-1, input_shape)
+            Input arrays for the current layer.
+
+        Returns
+        ---------
+        output: ndarray of shape (-1, output_shape)
+            Output arrays of the current layer.
+        """
         weighted_X = np.einsum('di, io -> doi', X, self.weights)
         activated = self._activate(weighted_X)
         return activated.sum(axis=-1)
     
     def deriv(self, X):
+        """
+        Get the derivitive of `activate` function with respect to input X.
+
+        Parameters
+        ----------
+        X: ndarray of shape (-1, input_shape)
+            Input arrays for the current layer.
+
+        Returns
+        ---------
+        output: ndarray of shape (-1, output_shape, input_shape)
+            Derivitive of `activate` function for each input array in X.
+        """
         weighted_X = np.einsum('di, io -> doi', X, self.weights)
         d = self._deriv(weighted_X)
         return np.einsum('doi, io -> doi', d, self.weights)
     
     def wt_deriv(self, X):
+        """
+        Get the derivitive of `activate` function with respect to layer weights
+        for input arrays in X.
+        
+        Parameters
+        ----------
+        X: ndarray of shape (-1, input_shape)
+            Input arrays for the current layer.
+
+        Returns
+        ---------
+        output: ndarray of shape (-1, output_shape, input_shape)
+            Derivitive of `activate` function wrt layer weights for each input array in X.
+        """
         weighted_X = np.einsum('di, io -> doi', X, self.weights)
         d = self._deriv(weighted_X)
         return np.einsum('doi, di -> doi', d, X)
 
     def copy(self):
+        """
+        Create a deep copy of the layer including all parameters and weights.
+        
+        Parameters
+        ----------
+        None
+
+        Returns
+        ---------
+        output: A copy of the layer with identical parameters and weights. 
+        """
         copy_layer = self.__class__(input_shape=self.input_shape, 
                                     output_shape=self.output_shape, 
                                     learning_rate=self.learning_rate)
@@ -43,9 +101,23 @@ class BaseLayer():
         return copy_layer
 
     def _activate(self, *args):
+        """Underlying activation function for layer type. 
+        Raises error if not overwritten in subclass.
+        
+        Raises
+        ------
+        NotImplementedError
+        """
         raise NotImplementedError("BaseLayer._activate must be overwritten.")
     
     def _deriv(self, *args):
+        """Underlying derivitive function for layer type. 
+        Raises error if not overwritten in subclass.
+        
+        Raises
+        ------
+        NotImplementedError
+        """
         raise NotImplementedError("BaseLayer._deriv must be overwritten.")
 
         
