423c2c8c96ed1cb245f1253e466d5e567e502230
Alex Klein <alexdavidklein@gmail.com>
2018-12-27 12:36:29 -0800

    revamp + implement layer.py

--- layer.py
+++ layer.py
@@ -1,18 +1,57 @@
-import numpy as np 
-from neuron import Neuron
+import numpy as np
 
 class Layer():
-    """Basic Layer class for use in Neural Network
-    """
-    def __init__(self, n=1000, **kwargs):
-        self.neurons = np.array([Neuron(**kwargs) for _ in range(n)])
+    def __init__(self, output_shape=10, input_shape=None, learning_rate=1e-3):
+        self.output_shape=output_shape
+        self.learning_rate=learning_rate
+        self.weights=None
+        if input_shape is None:
+            self.input_shape=None
+        else:
+            self.construct(input_shape) 
+        
+    def __getitem__(self, key):
+        return self.weights[key]
+    
+    def __call__(self, *args, **kwargs):
+        return self.activate(*args, **kwargs)
+    
+    def construct(self, dim):
+        self.input_shape=dim
+        self.weights=np.random.uniform(-1, 1, (self.input_shape, self.output_shape))
 
+    def activate(self, X):
+        weighted_X = np.einsum('di, io -> doi', X, self.weights)
+        activated = self._activate(weighted_X)
+        return activated.sum(axis=-1)
+    
+    def deriv(self, X):
+        weighted_X = np.einsum('di, io -> doi', X, self.weights)
+        d = self._deriv(weighted_X)
+        return np.einsum('doi, io -> doi', d, self.weights)
+    
+    def wt_deriv(self, X):
+        weighted_X = np.einsum('di, io -> doi', X, self.weights)
+        d = self._deriv(weighted_X)
+        return np.einsum('doi, di -> doi', d, X)
+
+    def copy(self):
+        copy_layer = self.__class__(input_shape=self.input_shape, 
+                                    output_shape=self.output_shape, 
+                                    learning_rate=self.learning_rate)
+        copy_layer.weights = self.weights.copy()
+        return copy_layer
+        
+class ReLU(Layer):
     def _activate(self, X):
-        """
-        Return activation output of all neurons in layer.
-        """
-        return np.apply_along_axis(lambda x: x._activate(X), -1, self.neurons)
-        
-    def _fit(self, X):
-        return np.apply_along_axis(lambda x: x._fit(X))
-        
+        return np.where(X < 0, 0, X)
+
+    def _deriv(self, X):
+        return np.where(X < 0, 0, 1)
+    
+class Dense(Layer):
+    def _activate(self, X):
+        return X
+
+    def _deriv(self, X):
+        return np.ones_like(X)
