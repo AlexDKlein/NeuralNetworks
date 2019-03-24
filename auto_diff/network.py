8d379c980ec533b0eff60f04d589ac7fcb3edd67
Alex Klein <alexdavidklein@gmail.com>
2019-03-24 13:00:53 -0700

    Add docstrings and toggle for batch shuffling.

--- auto_diff/network.py
+++ auto_diff/network.py
@@ -3,8 +3,26 @@ import tensorflow as tf
 tf.enable_eager_execution()
 
 class NN():
+    """Neural network api built over numpy and tensorflow. 
+    
+    Parameters
+    ------------
+    epochs: int, default=10
+        The number of epochs to run during a fit call.
+    batch_size: int, default=2
+        The batch size to use during fitting.
+    layers: None or List, default=None
+        If a list of layers is passed, they will be passed to add_layers upon initialization.
+    threshold: float, default=1e-3
+        Currently unimplemented. The minimum decrease in loss to trigger the early stop criteria.
+    shuffle_batches: bool, default=True
+        Whether to shuffle batches used during calls to fit. 
+        If False, adjustments will be made in a set order.
+    learning_rate: float, default=1e-1
+        Scaling factor for gradient descent. 
+    """
     def __init__(self, epochs=10, batch_size=2, layers=None, threshold=1e-3, shuffle_batches=True,
-                learning_rate=1e-3):
+                learning_rate=1e-1):
         self.epochs = epochs
         self.batch_size = batch_size
         self.layers = layers if layers is not None else []
@@ -16,22 +34,65 @@ class NN():
             self.add_layers(*layers)
     
     def fit(self, X, y=None):
+        """
+        Fits the model according to input X and target y.
+        Parameters
+        ----------
+        X: array-like
+            The independent variable.
+        y: array-like
+            The dependent variable.
+        Returns
+        ---------
+        output: self
+            The fitted model.
+        """
         X = tf.constant(X.reshape(-1, self.input_shape).astype(np.float32))
         y = tf.constant(y.reshape(-1, self.output_shape).astype(np.float32))
         self._back_propogate(X, y)
         return self
 
     def predict(self, X):
+        """
+        Predict values of the target variable for input array X.
+        Parameters
+        ----------
+        X: array-like
+            The independent variable.
+        Returns
+        ---------
+        output: np.ndarray
+            The model's prediction for inputs contained in X.
+        """
         X = tf.constant(X.reshape(-1, self.input_shape).astype(np.float32))
         return self._output(X).numpy()
     
     def _output(self, X):
+        """
+        Internal version of the predict method using tensorflow constants.
+        Parameters
+        ----------
+        X: tf.Tensor
+            The independent variable.
+        Returns
+        ----------
+        output: tf.Tensor
+            The model's prediction for inputs contained in X.
+        """
         output = X
         for layer in self.layers:
             output = layer._f(output @ layer.weights + layer.bias)
         return output
     
     def add_layers(self, *layers):
+        """
+        Add layers to the model.
+        Parameters
+        ----------
+        *layers: Activation layer objects
+            Layers to add.
+        
+        """
         for layer in layers:
             if len(self.layers) > 0:
                 layer._construct(self.layers[-1].output_shape) 
@@ -56,7 +117,8 @@ class NN():
     def _get_batches(self, n):
         n = int(n)
         batches = np.arange(n)
-        np.random.shuffle(batches)
+        if self.shuffle_batches:
+            np.random.shuffle(batches)
         for i in range(n//self.batch_size):
             yield batches[i::self.batch_size]
     
@@ -71,8 +133,10 @@ class NN():
     
     @property
     def input_shape(self):
+        """The expected size of input variables."""
         return self.layers[0].input_shape
     
     @property
     def output_shape(self):
+        """The expected size of target variables."""
         return self.layers[-1].output_shape
